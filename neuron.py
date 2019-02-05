d818fb3ec6acf5fbd1ab6929a5e91bb911e07175
birromer <hummescmpa@gmail.com>
2019-02-05 09:58:34 -0200

    corrected function names on neurons class

--- neuron.py
+++ neuron.py
@@ -1,30 +1,24 @@
 from math import exp
 
 class Neuron:
-    def __init__(self, weights, bias):
+    def __init__(self, weights): #add bias later
         self.weights = weights
-        # self.bias = bias
+        #self.bias = bias
 
-    def z(self, input): #sigmoid function
-        return (1 / 1 + exp(-input))
+    def activation_function(self, input): #sigmoid function
+        return 1.0 / (1.0 + exp(-(input)))
 
-    def y(self, inputs): #output
+    def z(self, inputs): #output
         output = 0
         for i in range(len(inputs)):
             output += (inputs[i] * self.weights[i])
+        #print(output)
         return output # + self.bias
 
-    def activation_function(self, inputs):
-        return sigmoid(output(inputs))
-
+    def y(self, inputs):
+        #print(self.z(self.y(inputs)))
+        return self.activation_function(self.z(inputs))
+    
 #    def updateWeights(learningRate, dEdy):
 #        for i in len(self.weights):
 #            weights[i] += learningRate * weight[i] * (target)
-
-
-if __name__ == "__main__":
-    inputs = [2, 5, 3]
-
-    neuron = Neuron([150, 50, 100], 0)
-
-    print(neuron.y(inputs))
