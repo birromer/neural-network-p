3cd1f61a5a8cebdd8d1d4d1174a0efa1d8191154
birromer <hummescmpa@gmail.com>
2019-02-05 09:38:48 -0200

    updated dense layer as notebook

--- denseLayer.py
+++ denseLayer.py
@@ -1,14 +1,27 @@
 from neuron import *
+from random import uniform
+
+def gen_rand_weights(size_vec):
+    weights = []
+    for _ in range(size_vec):
+        weights.append(uniform(0,1))
+    return weights
 
 class DenseLayer:
-    def __init__(self, num_of_inputs, num_of_neurons, base_weight):
+    def __init__(self, num_of_inputs, num_of_neurons):
         self.num_of_inputs = num_of_inputs
-        bw = [base_weight]
-        weights = bw * num_of_inputs
-        self.neurons = [Neuron(weights, 0) for i in range(len(num_of_neurons))]
+        self.neurons = []
+        for i in range(num_of_neurons):
+            weights = gen_rand_weights(num_of_inputs)
+            new_neuron = Neuron(weights)
+            self.neurons.append(new_neuron)
 
-    def feed_forward(inputs):
-        pass
+    def feed_forward(self, inputs):
+        output = []
+        for neuron in self.neurons:
+            output.append(neuron.y(inputs))
+            #print(neuron.activation_function(inputs))
+        return output
 
 
 if __name__ == "__main__":
