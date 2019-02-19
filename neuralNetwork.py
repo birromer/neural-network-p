5b8a2d5cca38307617ef40c168335d72ee6b7c38
birromer <hummescmpa@gmail.com>
2019-02-19 14:56:39 -0300

    updated network

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -4,7 +4,7 @@ class NeuralNetwork:
     def __init__(self, num_of_inputs, neurons_at_layer_1, neurons_at_layer_2):
         self. num_of_inputs = num_of_inputs
         layer_1 = DenseLayer(num_of_inputs, neurons_at_layer_1)
-        layer_2 = DenseLayer(neurons_at_layer_1, 10)    # 10 possible outputs in the one hot encoding
+        layer_2 = DenseLayer(neurons_at_layer_1, neurons_at_layer_2)
         self.layers = [layer_1, layer_2]
 
     def feed_forward(self, inputs):
