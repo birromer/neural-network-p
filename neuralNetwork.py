6e3de66ae31db8a19e4dbe4f6aa5679f13a5642d
birromer <hummescmpa@gmail.com>
2019-02-20 11:00:10 -0300

    started backpropagation and debugging

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -5,8 +5,28 @@ class NeuralNetwork:
         self. num_of_inputs = num_of_inputs
         layer_1 = DenseLayer(num_of_inputs, neurons_at_layer_1)
         layer_2 = DenseLayer(neurons_at_layer_1, neurons_at_layer_2)
+        
         self.layers = [layer_1, layer_2]
-
+        
+    def error_function(self, y, target):
+        t = [0 for _ in range(10)]
+        t[target] = 1
+        sum = 0
+        for i in range(len(y)):
+            diff = t[i] - y[i]
+            sum += (diff**2)
+        return sum
+        
+    def derivative_error_function(self, y, target):
+        t = [0 for _ in range(10)]
+        t[target] = 1
+        error_gradient = []
+        for i in range(len(y)):
+            partial_d = 2 * (t[i] - y[i])
+            #partial_d = -1 * (t[i] - y[i])
+            error_gradient.append(partial_d)
+        return error_gradient
+        
     def feed_forward(self, inputs):
         output_1 = []
         for neuron in self.layers[0].neurons:
@@ -15,6 +35,25 @@ class NeuralNetwork:
         for neuron in self.layers[1].neurons:
             output_2.append(neuron.y(output_1))
         return output_2
+    
+    def one_hot_encoding(x):
+        ohe = [0 for _ in range(x)]
+        ohe[x] = 1
+        return ohe
+    
+    def encoding_to_number(vec):
+        return vec.index(max(vec))
+    
+
+    def backpropagation(self, inputs, labels, output, learning_rate):
+        dEdy = network.derivative_error_function(output, label)
+        
+        for i in range(len(network.layers)-1, -1, -1):
+            for j in range(len(network.layers[i].neurons)):
+                network.layers[i].neurons[j].compute_dEdz(sample, dEdy)
+        
+        for i in range(len(network.layers)):
+            network.layers[i].update_weights(0.001)
 
 
 if __name__ == "__main__":
