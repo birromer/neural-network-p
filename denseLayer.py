f4c5b99baba3b23e5e05ee29b4896ed38044c64f
birromer <hummescmpa@gmail.com>
2019-03-02 15:00:42 -0300

    refactored to use numpy

--- denseLayer.py
+++ denseLayer.py
@@ -1,25 +1,17 @@
 from neuron import *
 from random import uniform
 
-def gen_rand_weights(size_vec):
-    weights = []
-    for _ in range(size_vec):
-        weights.append(uniform(-1,1))
-    return weights
-
 class DenseLayer:
     def __init__(self, num_of_inputs, num_of_neurons):
-        self.num_of_inputs = num_of_inputs
         self.neurons = []
         for i in range(num_of_neurons):
-            weights = gen_rand_weights(num_of_inputs)
-            bias = uniform(0,1)
+            weights = np.random.uniform(-1, 1, num_of_inputs)
+            bias = np.random.uniform(0,1,1)
             new_neuron = Neuron(weights, bias)
             self.neurons.append(new_neuron)
 
     def feed_forward(self, inputs):
         output = []
-        #print("number of neurons ", len(self.neurons))
         for neuron in self.neurons:
             output.append(neuron.y(inputs))
         return output
